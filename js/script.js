1c35d71be94a861f1aba95f360eb00f0b75dc4c5
elim322 <einer.lim322@gmail.com>
2018-10-19 15:52:59 -0700

    prettify files

--- js/script.js
+++ js/script.js
@@ -2,14 +2,11 @@ $(document).ready(function() {
   //url for api request
   $(".categories").on("change", function() {
     $(".gif-loader").css("display", "block");
-    $('.site-header').addClass("site-header-small");
-    
+    $(".site-header").addClass("site-header-small");
 
     let selectedStory = $(".categories").val();
 
-
     $(".article-boxes").empty();
-
 
     let url =
       "https://api.nytimes.com/svc/topstories/v2/" + selectedStory + ".json";
@@ -19,48 +16,50 @@ $(document).ready(function() {
         "api-key": "015e1e546b504dcab3dd3878ebbdc372"
       });
 
-    $.ajax({//ajax request 
+    $.ajax({
+      //ajax request
       url: url,
       method: "GET"
     })
-      .done(function(data) {//filter results for images only 
+      .done(function(data) {
+        //filter results for images only
         let onlyImg = data.results
           .filter(function(result) {
             return result.multimedia.length;
           })
           .slice(0, 12);
 
-
-
-        $.each(onlyImg, function(key, value) {//loop for pictures + articles
-          let html = "<a target='_blank' href=" + value.url + ">" +
+        $.each(onlyImg, function(key, value) {
+          //loop for pictures + articles
+          let html =
+            "<a target='_blank' href=" +
+            value.url +
+            ">" +
             "<div class='articles' id='articles' style='background: url(" +
-            value.multimedia[4].url + 
+            value.multimedia[4].url +
             "); background-size: cover; background-position: center;'>";
-          
-          html += "<p class='abstract'>" + value.abstract + "</p>";
-          
-          html += "</div>"; 
 
-          html += "</a>"; 
-        
-       
+          html += "<p class='abstract'>" + value.abstract + "</p>";
+
+          html += "</div>";
+
+          html += "</a>";
+
           $(".article-boxes").append(html);
-   //adding additional CSS
-          $('footer').addClass('footer-desktop');
-          $('body').addClass('body-results');
+          //adding additional CSS
+          $("footer").addClass("footer-desktop");
+          $("body").addClass("body-results");
         });
-       
       })
-      .fail(function(err) {//error notification for failing to load
+      .fail(function(err) {
+        //error notification for failing to load
         console.log("request failed");
         $(".article-boxes").append("<h3>Sorry there was an error</h3>");
       })
-      .always(function() {//always run gif loader whilst loading pictures 
+      .always(function() {
+        //always run gif loader whilst loading pictures
         console.log("always run");
         $(".gif-loader").css("display", "none");
-
-     
       });
   });
-});//end of doc ready
+}); //end of doc ready
